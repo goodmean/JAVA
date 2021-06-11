a1f8bf7b92583a0f925b1d24729d5188ee550ba0
SBS- <SBS-@SBS-85>
2021-06-11 12:31:00 +0900

    0611

--- JavaExam_2021_06_11_1/src/Main.java
+++ JavaExam_2021_06_11_1/src/Main.java
@@ -1,12 +1,8 @@
-// 문제 : `안녕하세요.`를 10번 출력해주세요.
-// 조건 : System.out.println은 딱 한번만 사용 가능
-// 조건 : \를 활용해서 줄바꿈 해주세요.
-// 힌트 : \n 은 줄바꿈을 의미합니다.
 
 class Main {
   public static void main(String[] args) {
-    // `\` <= 백스페이스 좌측 키
-    System.out.println("안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n");
-    int x = 1;
+    int a = 20;
+    int b = 30;
+    System.out.println(a+b);
   }
 }
