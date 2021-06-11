07b0cdb7e22d0990477999417cf97c2ef8c5969c
SBS- <SBS-@SBS-85>
2021-06-11 12:37:45 +0900

    0611

--- JavaExam_2021_06_11_1/src/Main.java
+++ JavaExam_2021_06_11_1/src/Main.java
@@ -3,7 +3,8 @@ class Main {
   public static void main(String[] args) {
     int x1 = 10;
     System.out.println(x1);
-    x1 = x1 + 1;
-    System.out.println(x1);
+    int x2;
+    x2 = x1 + 1;
+    System.out.println(x2);
   }
 }
