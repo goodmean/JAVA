90febc89f0498500c1a013247b9bb44f568093da
goodmean <zizigoo12@gmail.com>
2021-06-15 12:01:01 +0900

    9*9dan

---
+++ JAVA_Exam_2021_06_15/src/Main2.java
@@ -0,0 +1,28 @@
+// 문제 : 아래와 같이 출력해주세요.
+
+class Main2 {
+  public static void main(String[] args) {
+	String s;
+	
+	for(int dan = 2; dan <= 9; dan++) {
+		System.out.println(dan + "단");
+		for(int i = 1; i <=9; i++) {
+			System.out.println(dan + "x" + i + "=" + dan*i);
+		}
+		System.out.println();
+	}
+    
+    // 출력
+    /*
+    8 * 1 = 8
+    8 * 2 = 16
+    8 * 3 = 24
+    8 * 4 = 32
+    8 * 5 = 40
+    8 * 6 = 48
+    8 * 7 = 56
+    8 * 8 = 64
+    8 * 9 = 72
+    */
+  }
+}
