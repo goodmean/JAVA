3c7e2b60d78d8438bfb3df4d13b2024b60054fe6
goodmean <zizigoo12@gmail.com>
2021-06-16 12:52:06 +0900

    0616

--- JAVA_Exam_2021_06_15/src/Main2.java
+++ JAVA_Exam_2021_06_15/src/Main2.java
@@ -1,17 +1,17 @@
 // 문제 : 아래와 같이 출력해주세요.
 
 class Main2 {
-  public static void main(String[] args) {
-	
-	for(int dan = 2; dan <= 9; dan++) {
-		
-		System.out.println("==" + dan + "단" + "==");
-		for(int i = 1; i <=9; i++) {
-			
-			System.out.println(dan + "x" + i + "=" + dan*i);
+	public static void main(String[] args) {
+
+		for (int dan = 2; dan <= 9; dan++) {
+
+			System.out.println("==" + dan + "단" + "==");
+			for (int i = 1; i <= 9; i++) {
+
+				System.out.println(dan + "x" + i + "=" + dan * i);
+			}
+
+			System.out.println();
 		}
-		
-		System.out.println();
 	}
-  }
 }
