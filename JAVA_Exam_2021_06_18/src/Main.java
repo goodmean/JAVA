55e6006e991d20af46599f2650587409771d5fa1
goodmean <zizigoo12@gmail.com>
2021-06-18 12:56:44 +0900

    0618

--- JAVA_Exam_2021_06_18/src/Main.java
+++ JAVA_Exam_2021_06_18/src/Main.java
@@ -2,8 +2,28 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		System.out.println(3 + "살");
+		People p1 = new People();
+		p1.name = "김철수";
+		p1.age = 22;
+		p1.height = 177.7;
+		p1.introduce();
 		
-		System.out.printf("%d살, %.1f살, %s살", 3, 6.0, 'a');
+		People p2 = new People();
+		p2.name = "김영희";
+		p2.age = 20;
+		p1.height = 162.0;
+		p2.introduce();
+	}
+}
+
+class People{
+	String name;
+	int age;
+	double height;
+	
+	void introduce() {
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
 	}
 }
---
+++ JAVA_Exam_2021_06_18/src/Main2.java
@@ -0,0 +1,29 @@
+
+public class Main2 {
+	public static void main(String[] args) {
+		
+		Person a = new Person();
+		a.age = 20;
+		a.isMarried= false;
+		a.name = "Paul";
+		a.introduce();
+		a.숨쉬다();
+	}
+}
+
+class Person {
+	int age;
+	boolean isMarried;
+	String name;
+	
+	void introduce() {
+		System.out.println("== 자기소개 시작");
+		System.out.printf("이름 : %s\n", name);
+		System.out.printf("나이 : %d\n", age);
+		System.out.printf("기혼 여부 : %b\n", isMarried);
+	}
+	
+	void 숨쉬다(){
+		System.out.println("숨쉼");
+	}
+}
