4c5ed0fbab27035b76fff52c49bbb27f336a2941
goodmean <zizigoo12@gmail.com>
2021-08-09 11:56:34 +0900

    2068.최대수 구하기

--- JAVA_SW_EXAM/src/Main2068.java
+++ JAVA_SW_EXAM/src/Main2068.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 
 public class Main2068 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int a = 0;
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int temp = 0;
+			for(int i = 0; i < 10; i++) {
+				a = sc.nextInt();
+				if(temp < a) {
+					temp = a;
+				}
+			}
+			System.out.println("#" + tc + " " + temp);
+		}
 		
 	}
 }
